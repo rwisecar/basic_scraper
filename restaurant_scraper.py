23f5b67d04dad65383da36efe348f84b320922cb
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-08 15:20:36 -0800

    added remaining filter functions

--- restaurant_scraper.py
+++ restaurant_scraper.py
@@ -87,6 +87,58 @@ def clean_data(td):
         return u""
 
 
+def extract_restaurant_metadata(element):
+    """Store cleaned metadata as a dictionary."""
+    metadata_rows = element.find('tbody').findall(
+        has_two_tds, recursive=False
+    )
+    rdata = {}
+    current_label = ''
+    for row in metadata_rows:
+        key_cell, val_cell = row.find_all('td', recursive=False)
+        new_label = clean_data(key_cell)
+        current_label = new_label if new_label else current_label
+        rdata.setdefault(current_label, []).append(clean_data(val_cell))
+    return rdata
+
+
+def is_inspection_row(element):
+    """Filter for rows that contain keyword 'inspection'."""
+    is_tr = element.name == 'tr'
+    if not is_tr:
+        return False
+    td_children = element.find_all('td', recursive=False)
+    has_four = len(td_children) == 4
+    this_text = clean_data(td_children[0]).lower()
+    contains_word = 'inspection' in this_text
+    does_not_start = not this_text.startswith('inspection')
+    return is_tr and has_four and contains_word and does_not_start
+
+
+def extract_score_data(element):
+    """Take in a restaurant listing, apply filter to find inspection data."""
+    inspection_rows = element.find_all(is_inspection_row)
+    samples = len(inspection_rows)
+    total = high_score = average = 0
+    for row in inspection_rows:
+        strval = clean_data(row.find_all('td')[2])
+        try:
+            intval = int(strval)
+        except (ValueError, TypeError):
+            samples -= 1
+        else:
+            total += intval
+            high_score = intval if intval > high_score else high_score
+    if samples:
+        average = total/float(samples)
+    data = {
+        u'Average Score': average,
+        u'High Score': high_score,
+        u'Total Inspections': samples
+    }
+    return data
+
+
 if __name__ == "__main__":
     kwargs = {
         'Inspection_Start': '1/1/2016',
@@ -99,12 +151,7 @@ if __name__ == "__main__":
         html = create_html_file(get_inspection_page(**kwargs))
     doc = parse_source(html)
     listings = extract_data_listings(doc)
-    for listing in listings:
-        metadata = listing.find('tbody').find_all(
-            has_two_tds, recursive=False
-        )
-        for row in metadata:
-            for td in row.find_all('td', recursive=False):
-                print(repr(clean_data(td)))
-            print()
-        print()
+    for listing in listings[:5]:
+        metadata = extract_restaurant_metadata(listing)
+        score_data = extract_score_data(listing)
+        print(score_data)
